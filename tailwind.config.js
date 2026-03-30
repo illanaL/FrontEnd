ccbe809c9c49e11b62000976e06451290bbfc37e
illana lahiany <illanacv@gmail.com>
2026-03-30 18:08:40 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: [
+    "./**/*.html",
+    "./styles/**/*.{css,html}",
+    "./node_modules/flyonui/dist/js/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flyonui"),
+     require("flyonui/plugin")
+  ],
+}
+
